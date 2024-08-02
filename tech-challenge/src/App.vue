36ef9fcb3837e01a0fbd9c440dcdc1b1c36d7f12
Bart de Rijk <bart.derijk@solvimon.com>
2024-08-02 09:57:59 +0200

    inital commit

---
+++ tech-challenge/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="header">
+    <h1 class="text-3xl font-bold underline !text-red-500">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style>  
+#header {
+  color: hotpink;
+  text-align: center;
+  margin: 29px auto;
+}
+</style>
